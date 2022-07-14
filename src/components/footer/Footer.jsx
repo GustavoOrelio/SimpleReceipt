9c827ffe26eeb040959455a0ff5496b92f2cbd3d
Gustavo Orelio <oreliogustavo@gmail.com>
2022-07-14 15:29:28 -0300

    Definindo um rodapé

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <div className="Footer">
+                <h4>@CopyRight Gustavo Orelio</h4>
+            </div>
+        );
+    }
+}
+
+export default Footer;
